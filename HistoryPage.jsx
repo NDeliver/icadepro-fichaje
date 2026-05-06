4f1d4a8e515adbd46c05597eedd5f05153e5771e
NDeliver <TU_CORREO_GITHUB>
2026-05-06 19:03:39 +0100

    Añadir historial de fichajes

---
+++ HistoryPage.jsx
@@ -0,0 +1,119 @@
+import { useEffect, useState } from 'react';
+import { supabase } from '../services/supabase';
+
+function HistoryPage() {
+  const [checkins, setCheckins] =
+    useState([]);
+
+  useEffect(() => {
+    fetchCheckins();
+  }, []);
+
+  // OBTENER FICHAJES
+  const fetchCheckins = async () => {
+    const { data, error } =
+      await supabase
+        .from('checkins')
+        .select('*')
+        .order('created_at', {
+          ascending: false,
+        });
+
+    if (error) {
+      console.error(error);
+      return;
+    }
+
+    setCheckins(data);
+  };
+
+  return (
+    <div>
+      <h1>
+        Historial de Fichajes
+      </h1>
+
+      <div
+        style={{
+          marginTop: '20px',
+          backgroundColor: 'white',
+          padding: '20px',
+          borderRadius: '10px',
+          overflowX: 'auto',
+        }}
+      >
+        <table
+          style={{
+            width: '100%',
+            borderCollapse: 'collapse',
+          }}
+        >
+          <thead>
+            <tr>
+              <th style={thStyle}>
+                Código
+              </th>
+
+              <th style={thStyle}>
+                Curso
+              </th>
+
+              <th style={thStyle}>
+                Aula
+              </th>
+
+              <th style={thStyle}>
+                Tipo
+              </th>
+
+              <th style={thStyle}>
+                Fecha
+              </th>
+            </tr>
+          </thead>
+
+          <tbody>
+            {checkins.map((item) => (
+              <tr key={item.created_at}>
+                <td style={tdStyle}>
+                  {item.code}
+                </td>
+
+                <td style={tdStyle}>
+                  {item.course}
+                </td>
+
+                <td style={tdStyle}>
+                  {item.classroom}
+                </td>
+
+                <td style={tdStyle}>
+                  {item.type}
+                </td>
+
+                <td style={tdStyle}>
+                  {new Date(
+                    item.created_at
+                  ).toLocaleString()}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
+}
+
+const thStyle = {
+  textAlign: 'left',
+  padding: '12px',
+  borderBottom: '2px solid #ddd',
+};
+
+const tdStyle = {
+  padding: '12px',
+  borderBottom: '1px solid #eee',
+};
+
+export default HistoryPage;
