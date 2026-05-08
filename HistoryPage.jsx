71b5a8faaa1e75655e84a0eb429319fb03e73007
NDeliver <TU_CORREO_GITHUB>
2026-05-08 01:47:21 +0100

    Fix Canary timezone

--- HistoryPage.jsx
+++ HistoryPage.jsx
@@ -93,7 +93,17 @@ function HistoryPage() {
     return new Date(
       date
     ).toLocaleString(
-      'es-ES'
+      'es-ES',
+      {
+        timeZone:
+          'Atlantic/Canary',
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric',
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+      }
     );
   };
 
